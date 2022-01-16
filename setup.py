4425ae746a5fb9a44f096d489e8ea2b49b58feea
Piers14 <61315972+piers-hinds@users.noreply.github.com>
2022-01-16 21:06:55 +0000

    setup and version

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+from nmr.version import __version__
+
+setup(
+    name='nmr',
+    url='https://github.com/Piers14/numerai',
+    author='Piers Hinds',
+    author_email='pmxph7@nottingham.ac.uk',
+    packages=['nmr'],
+    install_requires=['numpy', 'torch', 'pandas', 'numerapi', 'sklearn'],
+    tests_require=['pytest'],
+    version=__version__,
+    license='MIT',
+    description='Helpers for numerai'
+)
